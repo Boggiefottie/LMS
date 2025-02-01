5c8c62b6fb031dcc73ca3b28798892bbc0dc97bf
bhavyapratapsingh <bhavyapratapsingh240203@gmail.com>
2025-02-01 06:10:05 +0530

    fifth commit

--- server/model/course.model.js
+++ server/model/course.model.js
@@ -1,29 +1,47 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 const courseSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
+    courseTitle:{
+        type:String,
+        required:true
     },
-    description: {
-        type: String,
-        required: true,
+    subTitle: {type:String}, 
+    description:{ type:String},
+    category:{
+        type:String,
+        required:true
     },
-    instructor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User', // Reference to the User model
-        required: true,
+    courseLevel:{
+        type:String,
+        enum:["Beginner", "Medium", "Advance"]
     },
-    studentsEnrolled: [
+    coursePrice:{
+        type:Number
+    },
+    courseThumbnail:{
+        type:String
+    },
+    enrolledStudents:[
         {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User', // Reference to the User model
-        },
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
     ],
-    createdAt: {
-        type: Date,
-        default: Date.now,
+    lectures:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Lecture"
+        }
+    ],
+    creator:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
     },
- }, { timestamps: true });
+    isPublished:{
+        type:Boolean,
+        default:false
+    }
+
+}, {timestamps:true});
 
 export const Course = mongoose.model("Course", courseSchema);
